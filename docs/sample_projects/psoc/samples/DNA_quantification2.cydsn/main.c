41f104db32c477059386411c865bca38e6fca80a
Omprakash <86110190+omprakashbanerjee@users.noreply.github.com>
2021-12-20 17:44:29 +0530

    DNA quantification project modified

--- docs/sample_projects/psoc/samples/DNA_quantification2.cydsn/main.c
+++ docs/sample_projects/psoc/samples/DNA_quantification2.cydsn/main.c
@@ -10,27 +10,16 @@
  * ========================================
 */
 #include "project.h"
-#include<stdio.h>
+
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
-    USBUART_1_Start(0,USBUART_1_5V_OPERATION);
-    while(USBUART_1_GetConfiguration()==0){}
+
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-    ADC_Start(); 
+
     for(;;)
     {
-        ADC_StartConvert();
-        ADC_IsEndConversion( ADC_WAIT_FOR_RESULT);
-        int val=ADC_GetResult32();
-        ADC_StopConvert();
-         char send[100];
-        sprintf(send,"%d\r\n",val);
-        
-        USBUART_1_PutString(send);
-        
-        CyDelay(50);   /* Place your application code here. */
-    
+        /* Place your application code here. */
     }
 }
 
