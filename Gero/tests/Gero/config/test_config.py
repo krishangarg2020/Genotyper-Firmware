ba66c4139f32e0c6fea2ed7c31344ccc59613e68
satyam <satyam@technoculture.io>
2022-05-20 11:13:54 +0530

    build(vscode): add launch.json for emulator target

--- Gero/tests/Gero/config/test_config.py
+++ Gero/tests/Gero/config/test_config.py
@@ -1,14 +1,11 @@
+# import pytest
 
-
-import pytest
-
-import config
-import gero.config
+from gero import config
 
 
 def test_name_not_on_robot():
     """test if IS_ROBOT is False"""
-    IS_ROBOT = config.IS_ROBOT
+    IS_ROBOT: bool = config.IS_ROBOT
     config.IS_ROBOT = False
 
     assert config.name() == 'gero-dev'
