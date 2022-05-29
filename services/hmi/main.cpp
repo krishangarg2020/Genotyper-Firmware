afcbb71b28c5411940a2575c3c433a069d157bb9
satyam <satyam@technoculture.io>
2022-05-29 12:51:39 +0530

    chore: add an empty qt5 project

--- services/hmi/main.cpp
+++ services/hmi/main.cpp
@@ -1,28 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-#include <QLocale>
-#include <QTranslator>
-
 int main(int argc, char *argv[])
 {
-    qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
-
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
 
     QGuiApplication app(argc, argv);
-
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "genetalker_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            app.installTranslator(&translator);
-            break;
-        }
-    }
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
