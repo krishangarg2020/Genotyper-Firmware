67d5fb7b353924a1d8b760ed2248abbb4d733b3f
Satyam Tiwary <satyam@technoculture.io>
2022-05-31 13:21:40 +0530

    build: remove accidental folder

--- Gero/src/Gero/config.py
+++
@@ -1,8 +0,0 @@
-from dynaconf import Dynaconf  # type: ignore
-
-settings = Dynaconf(
-    envvar_prefix="GERO",
-    # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
-    settings_files=['settings.toml', '.secrets.toml'],
-    # `settings_files` = Load these files in the order.
-)
